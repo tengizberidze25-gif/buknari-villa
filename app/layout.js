dd4963d3c1a7c73017c37c0e71604e9c38ae59ec
tengizberidze25-gif <tengizberidze25@gmail.com>
2026-07-03 16:56:52 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'Buknari Villa',
-  description: 'ვილების და სახლების გაქირავება ბუქნარში',
+  description: 'ვილების და სახლების გაქირავება ბუკნარში',
 };
 
 export default function RootLayout({ children }) {
