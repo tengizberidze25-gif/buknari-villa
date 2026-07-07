e9e76a551745eac33cc41b38003d1237bb303927
tengizberidze25-gif <tengizberidze25@gmail.com>
2026-07-07 21:52:48 +0400

    Update VillageSelect.js

--- app/VillageSelect.js
+++ app/VillageSelect.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 
-export default function VillageSelect({ initialValue }) {
+export default function VillageSelect({ initialValue, onChange }) {
   const [villages, setVillages] = useState([]);
   const [value, setValue] = useState(initialValue || '');
   const [loading, setLoading] = useState(true);
@@ -17,19 +17,23 @@ export default function VillageSelect({ initialValue }) {
       .finally(() => setLoading(false));
   }, []);
 
+  useEffect(() => {
+    if (onChange) onChange(value);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
   return (
     <div className="form-row">
-      <label>სოფელი / დაბა *</label>
+      <label>ქალაქი / სოფელი / დაბა *</label>
       <select
         name="village"
+        className="village-select"
         value={value}
         onChange={(e) => setValue(e.target.value)}
         required
         disabled={loading}
       >
-        <option value="">
-          {loading ? 'იტვირთება...' : 'აირჩიეთ...'}
-        </option>
+        <option value="">{loading ? 'იტვირთება...' : 'აირჩიეთ...'}</option>
         {villages.map((v) => (
           <option key={v.id} value={v.name}>
             {v.name}
